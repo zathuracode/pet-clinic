0f47eee6361b60bfa805190b51c6dea413a4dfdb
DIEGO GOMEZ <diegomezusb@gmail.com>
2021-04-27 16:08:55 -0500

    Corrección bug

--- ui-tests.sh
+++ ui-tests.sh
@@ -7,7 +7,7 @@ start_test()
     set -e
     docker pull devopsdojo/selenium-yb:latest
     docker create --name="selenium-testsuite" devopsdojo/selenium-yb:latest
-    sed '3q;d' /tmp/shortname.txt | sed 's/8080/9967/1' | xargs -I 'my_arg' sed -i 's#http://petclinic#my_arg#' src/test/selenium-robot/resources/resource.robot
+    sed '1q;d' /tmp/data.txt | sed 's/8080/9967/1' | xargs -I 'my_arg' sed -i 's#http://petclinic#my_arg#' src/test/selenium-robot/resources/resource.robot
     sleep 2s
     docker cp src/test/selenium-robot/PetclinicTestCases selenium-testsuite:/home/robotframework/src/test/selenium-robot
     docker cp src/test/selenium-robot/resources selenium-testsuite:/home/robotframework/src/test/selenium-robot
